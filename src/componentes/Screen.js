5be34650ab4faf45bace25176a405cdaa2bbf72f
ahiou <ahiou1ahiou@gmail.com>
2022-03-30 20:36:19 +0900

    Initial setup.

---
+++ src/componentes/Screen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+
+function Screen({ children }) {
+  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+  },
+});
+
+export default Screen;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Screen from "../componentes/Screen";
+
+function HomeScreen(props) {
+  return <Screen></Screen>;
+}
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
